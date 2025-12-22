97d916eda100f09ae9b80f922f9db341d47dd8ca
Muhammad Abdullah <abdullah.muhammad.xyz@gmail.com>
2025-12-22 14:16:13 +0500

    First Commit

---
+++ Components/blogitem.jsx
@@ -0,0 +1,40 @@
+import { assets, blog_data } from "@/Assets/assets";
+import Image from "next/image";
+import React from "react";
+
+
+const BlogItem = ({title, description, category, image}) => {
+    return (
+        <div className='max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000] cursor-pointer'>
+            <Image src={image} alt='' width={400} height={400} className='border-b border-black'/>
+            <p className='ml-5 mt-5 px-1 inline-block bg-black text-white text-sm'>{category}</p>
+            <div className="p5">
+                <h5 className='ml-5 mt-5 mb-3 text-lg font-bold tracking-tight text-gray-900'>{title}</h5>
+                <p className='ml-5 mb-3 text-gray-700 text-sm tracking-tight'>{description}</p>
+                <div className='inline-flex items-center py-2 font-semibold text-center ml-5'>
+                    Read More <Image src={assets.arrow} alt='' className='inline-block ml-2' width={12}/>
+                </div>
+            </div>
+
+
+
+
+        </div>
+
+
+
+    )
+
+
+
+
+
+
+
+
+
+
+
+}
+
+export default BlogItem;
